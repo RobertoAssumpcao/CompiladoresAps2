aabc306ccdd95388eddb98f080dbc8ecaae03f8c
Roberto de Paiva Assumpção <robertopaiva26@outlook.com>
2022-11-24 21:38:23 -0300

    loop de varredura

--- aps2/main.js
+++ aps2/main.js
@@ -1,18 +1,88 @@
+function ObjCodigoAnalisado(token, codigo) {
+    this.token = token;
+    this.codigo = codigo;
+};
+
 function separadorCodigo() {
     let entrada = document.getElementById('codigo_fonte').value;
-
-    entrada = entrada.split(";").join("");
-    entrada = entrada.split("\n").join(" ");
-    entrada = entrada.split(" ");
+    entrada = entrada.split("\n").join(" ").split(" ");
     return entrada;
 }
 
 function validaIdentificador() {
-    const codigo = separadorCodigo();
-    const regex = /^[a-z]\w*$/;
-    const identificador = regex.exec(codigo);
-    if (!identificador)
-        window.alert(codigo + " Não é IDENT");
-    else
-        window.alert("IDENT: " + codigo);
+    const codigoEmAnalise = separadorCodigo();
+    const regex = [/^[a-z]\w*$/,,, /^[1-9]\w*$/];
+    let identificador = [];
+    let codigoAnalisado = [];
+
+    // Identificador
+    for (i = 0; i < codigoEmAnalise.length; i++) {
+        identificador.push(regex[0].exec(codigoEmAnalise[i]));
+        if (identificador[i] == null) {
+            continue;
+        }
+        else {
+            codigoAnalisado.push(new ObjCodigoAnalisado("Identificador", codigoEmAnalise[i]));
+        }
+    }
+    
+    identificador = [];
+    // Atribuição // teste 1 a 9
+    for (i = 0; i < codigoEmAnalise.length; i++) {
+        identificador.push(regex[1].exec(codigoEmAnalise[i]));
+        if (identificador[i] == null) {
+            continue;
+        }
+        else {
+            codigoAnalisado.push(new ObjCodigoAnalisado("Atribuição", codigoEmAnalise[i]));
+        }
+    }
+
+    identificador = [];
+    //Relacionais
+    for (i = 0; i < codigoEmAnalise.length; i++) {
+        identificador.push(regex[2].exec(codigoEmAnalise[i]));
+        if (identificador[i] == null) {
+            continue;
+        }
+        else {
+            codigoAnalisado.push(new ObjCodigoAnalisado("Relacionais", codigoEmAnalise[i]));
+        }
+    }
+
+    identificador = [];
+    //Inteiro
+    for (i = 0; i < codigoEmAnalise.length; i++) {
+        identificador.push(regex[3].exec(codigoEmAnalise[i]));
+        if (identificador[i] == null) {
+            continue;
+        }
+        else {
+            codigoAnalisado.push(new ObjCodigoAnalisado("Inteiro", codigoEmAnalise[i]));
+        }
+    }
+
+    identificador = [];
+    //Numeros reais
+    for (i = 0; i < codigoEmAnalise.length; i++) {
+        identificador.push(regex[4].exec(codigoEmAnalise[i]));
+        if (identificador[i] == null) {
+            continue;
+        }
+        else {
+            codigoAnalisado.push(new ObjCodigoAnalisado("Numeros reais", codigoEmAnalise[i]));
+        }
+    }
+
+    identificador = [];
+    //String
+    for (i = 0; i < codigoEmAnalise.length; i++) {
+        identificador.push(regex[5].exec(codigoEmAnalise[i]));
+        if (identificador[i] == null) {
+            continue;
+        }
+        else {
+            codigoAnalisado.push(new ObjCodigoAnalisado("String", codigoEmAnalise[i]));
+        }
+    }
 }
